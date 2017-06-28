66b29c00a444f0498b9f3224bf5beb96e4feabe4
haglermgp <hagler.mijail@gmail.com>
2017-06-28 05:21:37 -0500

    [update] rotate, move not all

--- game/js/app.js
+++ game/js/app.js
@@ -2,50 +2,6 @@ var inicio  = () =>{
 	let c = document.getElementById("Canvas");
 	let ctx = c.getContext("2d");
 
-	//Creando Usuario
-
-	// //USER DOWN
-	// function usuario(){
-	// 	camino();
-	// 	ctx.beginPath();
-	// 	ctx.moveTo(155,235); //(x+5 , y+5) esquina base 1 derecha
-	// 	ctx.lineTo(185,235); //(x+35, y+5) esquina base 2 izquierda
-	// 	ctx.lineTo(170,265); //(x+20, y+35) esquina direccion down
-	// 	ctx.fillStyle='black';
-	// 	ctx.fill();
-	// }
-
-	// //USER UP
-	// function usuario(){
-	// 	camino();
-	// 	ctx.beginPath();
-	// 	ctx.moveTo(185,265); //(x+35 , y+35) esquina base 1 derecha
-	// 	ctx.lineTo(155,265); //(x+5, y+35) esquina base 2 izquierda
-	// 	ctx.lineTo(170,235); //(x+20, y+5) esquina direccion up
-	// 	ctx.fillStyle='black';
-	// 	ctx.fill();
-	// }
-
-	// //USER RIGHT
-	function usuarioRight(){
-		ctx.beginPath();
-		ctx.moveTo(155,235); //(x+5 , y+5) esquina base 1 derecha
-		ctx.lineTo(155,265); //(x+5, y+35) esquina base 2 izquierda
-		ctx.lineTo(185,250); //(x+35, y+20) esquina direccion right
-		ctx.fillStyle='black';
-		ctx.fill();
-	}
-
-	// //USER LEFT
-	function usuarioLeft(){
-		ctx.beginPath();
-		ctx.moveTo(185,235); //(x+35 , y+5) esquina base 1 derecha
-		ctx.lineTo(185,265); //(x+35, y+35) esquina base 2 izquierda
-		ctx.lineTo(155,250); //(x+5, y+20) esquina direccion left
-		ctx.fillStyle='black';
-		ctx.fill();
-	}
-
 	//Creando Camino
 	function camino(){
 		for(let i = 0 ; i < 6 ; i++)
@@ -74,35 +30,103 @@ var inicio  = () =>{
 	}
 
 	// NOTE: FUNCTIONS COMANDS
+
+	//ROTATE
+
+	var startX = 240;
+	var startY = 240;
+
+	var a = -20;
+	var b = -20;
+
+	var degrees=0;
+
+	function Rotated(color, x, y, a, b) {
+
+			// first save the untranslated/unrotated context
+			ctx.save();
+
+			ctx.beginPath();
+			// move the rotation point to the center of the rect
+			ctx.translate(x - a/2 , y - b/2);
+			// rotate the rect
+			ctx.rotate(degrees * Math.PI / 180);
+
+			// draw the rect on the transformed context
+			// Note: after transforming [0,0] is visually [x,y]
+			//       so the rect needs to be offset accordingly when drawn
+			ctx.moveTo(a+5 , b+5); //(x+5 , y+5) esquina base 1 derecha
+			ctx.lineTo(a+5, b+35); //(x+5, y+35) esquina base 2 izquierda
+			ctx.lineTo(a+35, b+20); //(x+35, y+20) esquina direccion right
+
+			ctx.fillStyle = color;
+			ctx.fill();
+
+			// restore the context to its untranslated/unrotated state
+			ctx.restore();
+
+	}
+
+	function Rotate(direction) {
+		if (direction == 'left') {
+			degrees -= 90 // decrease cuantity of degrees, so, the direction of triangle
+			return degrees = ((degrees <= -360) ? 0 : degrees) // this conditional reestart the value of degrees if is less than -360
+			// console.log('llego a -360 y reestart a 0:' + degrees);
+
+		}else if (direction == 'right') {
+			degrees += 90 // increase cuantity of degrees, so, the direction of triangle
+			return degrees = ((degrees >= 360) ? 0 : degrees) // this conditional reestart the value of degrees if is more than +360
+			// console.log('llego a +360 y reestart a 0:' + degrees);
+		}
+
+		//this part reload initial degrees value when the rotation pass by position intial
+	}
+
+	degrees
+
+
 	function Move() {
-		this.left 	= left
-		this.right  = right
-		this.up		  = up
-		this.down		= down
+
+	//MOVEMENT IN X
+		if (degrees == 0) {
+			startX += 40
+		}else if ( (degrees == -180) || (degrees == 180) ) {
+			startX -= 40
+		}else {
+			return startX += 0
+		}
+
+	//MOVEMENT IN Y
+		if ((degrees == 90) || (degrees == -270)) {
+		 startY = startY + 40
+		}else if ((degrees == 270) || (degrees == -90) ) {
+		 startY = startY - 40
+		}else {
+		 startY += 0
+		}
+
+		return startY
+		return startX
+
 	}
 
-	function left() {
-
-	}
-
-	function right() {
-
-	}
-
-	function up() {
-
-	}
-
-	function down() {
-
-	}
 
 
 	camino();
+	// draw a rotated rect
+
+
+	// Rotate('left')
+
+	Rotate('left')
+	console.log('determinamos la direccion: ' + degrees);
+	Move()
+		debugger;
+	Move()
+	Move()
+
+
+	Rotated("yellow", startX, startY, a, b);
+
 }
 inicio();
-
-
- 	//else {
-			//alert("perdistes :(")
-		//}
